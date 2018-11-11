60879b1018726fcadffae318d737d59595ad2d52
nashvent <anibal.ventura@ucsp.edu.pe>
2018-11-11 14:31:58 -0500

    Glut agregado

--- main.cpp
+++ main.cpp
@@ -1,330 +1,263 @@
+#define GLUT_DISABLE_ATEXIT_HACK
 #include <bits/stdc++.h>
+#include <math.h>
+#include <cmath>
+#include <GL/glut.h>
+#include "rtree.h"
+#define KEY_ESC 27
+
 using namespace std;
-typedef float TypeData;
-typedef vector<float> Data;
-typedef vector<Data> vData;
-/*
-class IR{ //Index record / No implementado por el momento
-};
-*/
-
-float area(vData mm){
-    float areaCalc=1;
-    for(int i=0;i<mm.size();i++){
-        areaCalc=areaCalc*(mm[i][1]-mm[i][0]);
-    }
-    return areaCalc;
+/*FUnciontes importantes */
+vector<string> splitString(string str){
+    vector<string>vect;
+    string word;
+    stringstream stream(str);
+    while( getline(stream, word, ',') )
+        vect.push_back(word);
+    return vect;
 }
 
-vData makeRectangle(vData E1,vData E2){
-    vData R;
-    for(int i=0;i<E1.size();i++){
-        vector<float>tempDim(2);
-        if(E1[i][0]>E2[i][0])
-            tempDim[0]=E2[i][0];
-        else
-            tempDim[0]=E1[i][0];
+/*Variables globales*/
+RTree *rt;
+vector<Nodo*> puntos;
+bool ial = false;
+bool showCircle=false;
 
-        if(E1[i][1]<E2[i][1])
-            tempDim[1]=E2[i][1];
-        else
-            tempDim[1]=E1[i][1];
-        R.push_back(tempDim);
-    }
-    return R;
+
+double radio=0,calx,caly,centrox,centroy;
+
+void adaptRawPoint(int &x,int &y)
+{
+	x = x-300;
+	y = 300-y;
+}
+
+//dibuja un simple gizmos
+void displayGizmo()
+{
+	glBegin(GL_LINES);
+	glColor3d(0,255,0);
+	for(unsigned i=0;i<rt->allRectangles.size();i++){
+		Data xrect=rt->allRectangles[i][0];
+		Data yrect=rt->allRectangles[i][1];
+		//printVData(rt->allRectangles[i]);
+		glVertex2d(xrect[0],yrect[0]);
+		glVertex2d(xrect[1],yrect[0]);
+		
+		glVertex2d(xrect[1],yrect[0]);
+		glVertex2d(xrect[1],yrect[1]);
+		
+		glVertex2d(xrect[1],yrect[1]);
+		glVertex2d(xrect[0],yrect[1]);
+		
+		glVertex2d(xrect[0],yrect[1]);
+		glVertex2d(xrect[0],yrect[0]);
+	}
+    glEnd();
+
+	glPointSize(3);
+	glBegin(GL_POINTS);
+	glColor3d(255,0,0);
+	for(unsigned i=0;i<rt->allPoints.size();i++){	
+		Data pnt=rt->allPoints[i];
+		glVertex2d(pnt[0],pnt[1]);
+	}
+	glEnd();
+
+	/*glPointSize(2);
+	glBegin(GL_POINTS);
+	glColor3d(255,255,0);
+	for(unsigned i=0;i<busqueda.size();i++)
+	{	
+		glVertex2d(busqueda[i].x,busqueda[i].y );
+	}
+	glEnd();*/
+	/////////////////////////////////////////////
+	/*if(Ra.size()!=0 and Ra[0].x == centrox and  Ra[0].y == centroy)
+	{
+		glPointSize(2);
+		glBegin(GL_POINTS);
+		glColor3d(0,0,225);
+		for(unsigned i=0;i<Ra.size();i++)
+		{	
+			glVertex2d(Ra[i].x,Ra[i].y);
+		}
+
+
+		glEnd();
+	}*/
+	//////////////////////////////////////////////
+	/*if(showCircle)
+	{	
+		glBegin(GL_POINTS);
+		glColor3d(1.0,0.5,0.0);
+		radio=60;
+		for(double i=0;i<15;i=i+0.01)
+		{
+			calx = centrox + radio * cos(i);
+			caly = centroy + radio * sin(i);
+			glVertex2d(calx,caly);
+		}
+		glEnd();
+
+		qt->buscarCirculo(Punto(centrox,centroy),radio);
+		
+	}*/
+
+	//qt->PertCirculo(qt->m_head,Ra[0].x,Ra[0].y,radio);
+	/*EPuntos.clear();
+	qt->drawCirculo(centrox,centroy,radio);
+	//qt->areaCirculo(centrox,centroy,radio);
+
+	//qt->DistRadio(qt->m_head,radio,centrox,centroy);
+	*/
+	glPointSize(2);
+	glBegin(GL_POINTS);
+	glColor3d(1.0,1.0,0.0);
+	
+	glEnd();
+	
+}
+
+void OnMouseClick(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		adaptRawPoint(x,y);
+		//Punto pt(x,y);
+		//qt->insert(pt);
+		//qt->draw();
+	}
+	else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		showCircle=!showCircle;
+		adaptRawPoint(x,y);
+		centrox=x;
+		centroy=y;
+		//busqueda.clear();
+	}
+}
+
+void OnMouseMotion(int x, int y)
+{ 
+	adaptRawPoint(x,y);
+	centrox = x;
+	centroy = y;
+	
+}
+
+void idle()
+{ 
+	glutPostRedisplay();
+}
+
+//funcion llamada a cada imagen
+void glPaint(void) {
+	//El fondo de la escena al color initial
+	glClear(GL_COLOR_BUFFER_BIT); //CAMBIO
+	glLoadIdentity();
+	//glOrtho(xmin,xmax,ymin,ymax,-1.0f,1.0f);
+	glOrtho(-300.0f,300.0f,-300.0f,300.0f,-1.0f,1.0f);
+
+	//lsqt->draw();
+	displayGizmo();
+
+	//dldraw();
+	//doble buffer, mantener esta instruccion al fin de la funcion
+	glutSwapBuffers();
+}
+
+//
+//inicializacion de OpenGL
+//
+void init_GL(void) 
+{
+	//Color del fondo de la escena
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //(R, G, B, transparencia) en este caso un fondo negro
+
+	//modo projeccion
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+}
+
+//en el caso que la ventana cambie de tama�o
+GLvoid window_redraw(GLsizei width, GLsizei height) {
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glOrtho(-300.0f,300.0f,-300.0f,300.0f,-1.0f,1.0f);
+	//glOrtho(xmin,xmax,ymin,ymax,-1.0f,1.0f);
+}
+
+GLvoid window_key(unsigned char key, int x, int y) {
+	switch (key) {
+	case KEY_ESC:
+		exit(0);
+		break;
+
+	default:
+		break;
+	}
+
 }
 
 
-struct Nodo{
-    bool isLeaf,wasSplit,isData;
-    vData tuples; //tuplas [tupla,tupla,tupla] || [x,y,z,r,...],[x,y,z,r,...],[x,y,z,r,...]
-    vData I; //Limites del rectangulo I Nodo x:[min,max] y:[min,max] z:[min,max] r:[min,max]
-    vector<Nodo*>child; // [Nodo1,Nodo2,Nodo3,Nodo4]
-    int dim;
-    Nodo *parent;
-    Nodo(int n_dim,bool leaf=false){ //Nodo (3,true) is Leaf && Nodo(3,false) is Data
-        dim=n_dim;
-        isLeaf=leaf;
-        isData=!leaf;
-        //parent=n_parent;
-        I.resize(n_dim);
+void normalizar(float &x,float a,float b,float xmax,float xmin){
+	x=a+((x-xmin)*(b-a))/(xmax-xmin);
+}
+
+
+
+
+int main(int argc, char* argv[]){
+    //srand (time(NULL));
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600,600); 
+	glutInitWindowPosition(400, 80); 
+	glutCreateWindow("Rtree"); 
+
+	init_GL(); 
+
+	glutDisplayFunc(glPaint);
+	glutReshapeFunc(&window_redraw);
+	// Callback del teclado
+	glutKeyboardFunc(&window_key);
+	glutMouseFunc(&OnMouseClick);
+	glutMotionFunc(&OnMouseMotion);
+	glutIdleFunc(&idle);
+
+
+    int dim=2;
+    rt=new RTree(dim,2,4); //(dimension,m,M)
+    vData vrd;
+	float xmax=0,xmin=100000;
+	float ymax=0,ymin=100000;
+	for(int i=0;i<10;i++){
+        Data rnd=randomData(dim);
+        vrd.push_back(rnd);	
+		if(rnd[0]>xmax) xmax=rnd[0];
+		if(rnd[0]<xmin) xmin=rnd[0];
+		if(rnd[1]>ymax) ymax=rnd[1];
+		if(rnd[1]<ymin) ymin=rnd[1];
+	}
+
+    for(int i=0;i<vrd.size();i++){
+		
+		normalizar(vrd[i][0],-300,300,xmax+1,xmin-1);
+		normalizar(vrd[i][1],-300,300,ymax+1,ymin-1);
+        Nodo *tmp=new Nodo(dim,vrd[i]);
+        //printData(rnd);
+        cout<<"##################"<<endl;
+        cout<<"insert: ";
+        printData(vrd[i]);
+        rt->insert(tmp);
+        //rt->print();
     }
-    bool overlap(vData pI){
-        for(int i=0;i<dim;i++){
-            if(I[i][0]>pI[i][0] or pI[i][1]>I[i][1]){
-                return false;
-            }
-        }
-        return true;
-    }
+	rt->getRectangles();
+    //printVData(vrd);
 
-    int exist(vData pI){
-        for(int i=0;i<child.size();i++){
-            if(child[i]->I==pI){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    bool deleteChild(Nodo*H){
-        for(int i=0;i<child.size();i++)
-            if(child[i]==H){
-                child.erase(child.begin()+i);
-                return true;
-            }   
-        return false;
-    }
-
-    void addEntry(Nodo *E){
-        //vData nI=makeRectangle(I,E->I);
-        //I=nI;
-        child.push_back(E);
-        E->parent=this;
-    }
-
-    bool searchData(Data T){
-        if ( std::find(tuples.begin(), tuples.end(), T) != tuples.end() )
-            return true;
-        return false;
-    }
-    
-
-    float calcArea(vData dt){
-        vData nArea=I;
-        for(int i=0;i<dim;i++){
-            if(nArea[i][0]>dt[i][0]){
-                nArea[i][0]=dt[i][0];
-            }
-            if(nArea[i][1]<dt[i][1]){
-                nArea[i][1]=dt[i][0];
-            }
-        }
-        return area(nArea)-area(I);
-    }
-
-    float updateRectangleI(){
-        for(int i=0;i<child.size();i++){
-            for(int j=0;j<dim;j++){
-                if(child[i]->I[j][0] < I[j][0] ){
-                    I[j][0]=child[i]->I[j][0];
-                }
-                if(child[i]->I[j][1] > I[j][1] ){
-                    I[j][1]=child[i]->I[j][1];
-                }
-            }
-        }
-    }
-
-};
-
-
-struct RTree{
-    int M,m;
-    int dim;
-    Nodo *root;
-    RTree(int n_dim,int n_m,int n_M){
-        M=n_M;
-        m=n_m;
-        root=new Nodo(n_dim,true);
-        dim=n_dim;
-    }
-
-    bool search(Nodo *&p,vData pI){ //Buscar un punto o un rectangulo
-        // S2 /////////
-        if(p->isLeaf){
-            return true;
-        }
-        // S1 ///////
-        for(int x=0;x<p->child.size();x++){
-            Nodo*currChild=p->child[x];
-            if(currChild->overlap(pI)){
-                search(currChild,pI);
-
-            }
-        }
-        return false;
-    }
-
-    bool insert(Nodo *E){ 
-        Nodo* L,*LL;
-        // I1 ////////
-        chooseLeaf(E->I,L);
-
-        // I2 ///////
-        L->addEntry(E);
-        if(L->tuples.size()<=M){
-            return true;
-        }
-        else{
-            splitNode(L,LL);
-        }
-        // I3 ///////
-        adjustTree(L,LL); // SI no hubo split LL==NULL
-        // I4 //////
-        if(L==root and LL!=NULL){
-            Nodo* tempRoot=new Nodo(dim,false);
-            tempRoot->addEntry(L);
-            tempRoot->addEntry(LL);
-            root=tempRoot;
-        }
-        return true;
-    }
-
-    void chooseLeaf(vData E,Nodo *N){
-        // CL1 ////////
-        N=root;
-        // CL2  ///////
-        while(!N->isLeaf){
-            float tempArea=10000;
-            Nodo *TN;
-            for(int i=0;i<N->child.size();i++){
-                float nTempArea=area(makeRectangle(E,N->child[i]->I));
-                // CL3 /////
-                if(tempArea>nTempArea){
-                    TN=N->child[i];
-                    tempArea=nTempArea;
-                }
-            }
-            // CL4 /////
-            N=TN;
-        }
-        return;
-    }
-
-    void adjustTree(Nodo* &N,Nodo*&NN){ // Expand tree 
-        // AT1 | ET1 ////////
-            /*Nodo *N=L;Nodo *NN=LL;*/
-            /* Paso AT1 en la declaracion*/
-        // AT2 | ET2 ////////
-        while(N!=root){
-            // AT3 | ET3x ///////
-            Nodo *P=N->parent;
-            P->isLeaf=false;
-            for(int i=0;i<dim;i++){
-                if(N->I[i][0] < P->I[i][0])
-                    P->I[i][0] = N->I[i][0];
-                if(N->I[i][1] > P->I[i][1])
-                    P->I[i][1] = N->I[i][1];
-            }   
-            // AT4 | ET4 ////////
-            Nodo *PP=NULL;
-            if(NN!=NULL){
-                P->addEntry(NN);
-                if(P->child.size()>M){
-                    splitNode(P,PP);
-                }
-            }
-            // AT5 | ET5 ////////
-            N=P;
-            NN=PP;
-        }
-        return;
-    }
-
-    //Quadratic Split
-    void splitNode(Nodo* &G1,Nodo* &G2){
-        // QS1 ////////////
-        Nodo *E1,*E2;
-        float Ed1,Ed2;
-        vector<Nodo*>LP;
-        LP=G1->child;
-        bool checkLeaf=G1->isLeaf;
-        pickSeeds(LP,E1,E2);
-        G1=new Nodo(dim,checkLeaf); //Nodos como grupos G1, G2;
-        G2=new Nodo(dim,checkLeaf);
-        G1->addEntry(E1);
-        G2->addEntry(E2);
-        // QS2 //////////
-        while(LP.size()>0){
-            if(G1->child.size()>=m or G2->child.size()>=m and(G1->child.size()!=G2->child.size())){
-                if(G1->child.size()<m ){
-                    G1->child.insert( G1->child.end(), LP.begin(), LP.end());
-                    break;
-                }
-                if(G2->child.size()<m){
-                    G2->child.insert( G2->child.end(), LP.begin(), LP.end());
-                    break;
-                }
-                
-            }
-            // QS3 ////////
-            pickNext(LP,G1,G2);
-        }
-        return;
-    }
-
-    void pickSeeds(vector<Nodo*>&LP,Nodo* &E1,Nodo* &E2){
-        float d=0;
-        int indxE1,indxE2;
-        // PS1 //////////////
-        for(int x1=0;x1<LP.size();x1++){
-            for(int x2=0;x2<LP.size();x2++){
-                if(x1!=x2){
-                    vData J=makeRectangle(LP[x1]->I,LP[x2]->I);
-                    float td=area(J)*area(LP[x1]->I)*area(LP[x2]->I);
-                    // PS2 /////////////////////
-                    if(td>d){
-                        E1=LP[x1];
-                        E2=LP[x2];
-                        d=td;
-                        indxE1=x1;
-                        indxE2=x2;
-                    }
-                }
-            }
-        }
-        /*Remover los seleccionados del grupo*/
-        if(indxE1>indxE2){
-            swap(indxE1,indxE2);
-        }
-        LP.erase(LP.begin()+indxE2);
-        LP.erase(LP.begin()+indxE1);
-    }
-
-    void pickNext(vector<Nodo*>&LP,Nodo* &G1,Nodo* &G2){
-        int indxE1,indxE2;
-        float dG1=1000000,dG2=1000000;
-        vData G1,G2;
-        float aG1=area(G1->I);
-        float aG2=area(G2->I);
-        vData ntG1,ntG2;
-        // PN1 ///////
-        for(int i=0;i<LP.size();i++){
-            vData tG1=makeRectangle(G1->I,LP[i]->I);
-            vData tG2=makeRectangle(G2->I,LP[i]->I);
-            float tdG1=area(tG1)-aG1;
-            float tdG2=area(tG2)-aG2;
-            if(tdG1<dG1){
-                indxE1=i; //Indice en LP
-                dG1=tdG1; //Diferencia agregando la nueva entrada
-                ntG1=tG1; //nuevo Rectangulo ->  I
-            }
-            if(tdG2<dG2){
-                indxE2=i;
-                dG2=tdG2;
-                ntG2=tG2;           
-            }
-            
-        }
-
-        // PN2 //////////////
-        if(dG1<dG2){
-            G1->addEntry(LP[indxE1]);
-            LP.erase(LP.begin()+indxE1);
-            return;
-        }
-        else{   
-            G2->addEntry(LP[indxE2]);
-            LP.erase(LP.begin()+indxE2);
-            return;
-        }
-    }
-};
-
-
-
-int main(){
-    
+	glutMainLoop();
     return 0;
 }
